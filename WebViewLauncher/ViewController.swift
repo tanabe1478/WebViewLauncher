7308218ab2aa0495fc81d2b031e95f1156e41ce2
tanabe.nobuyuki <tanabe.1478@gmail.com>
2023-09-28 20:51:03 +0900

    first commit

--- WebViewLauncher/ViewController.swift
+++ WebViewLauncher/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
@@ -13,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func didTappedSFSafariViewButton(_ sender: Any) {
+        let url = URL(string: "http://google.co.jp")!
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true)
+    }
+    
+    @IBAction func didTappedWKWebViewButton(_ sender: Any) {
+    }
+    
 }
 
