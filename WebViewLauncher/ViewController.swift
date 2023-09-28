011f8ba4eadc1bcf09a177b35c78a48fb0caad18
tanabe.nobuyuki <tanabe.1478@gmail.com>
2023-09-28 13:25:28 +0900

    Initial Commit

---
+++ WebViewLauncher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebViewLauncher
+//
+//  Created by tanabe.nobuyuki on 2023/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
